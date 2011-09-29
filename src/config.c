f20dd432e7d85ddbe429af435923070a8f3a61d1
Matvey Aksenov <matvey.aksenov@gmail.com>
2011-09-29 23:08:35 +0400

    Fix possible memory leak (if config contents invalid record).

--- src/config.c
+++ src/config.c
@@ -86,17 +86,27 @@ static int is_not_valid_config_record( config_record_t * record )
 	return ( !record->command || !record->pattern || !record->source_path || !record->destination_path );
 }
 
+static void free_config_record( config_record_t * config_record )
+{
+	if ( config_record->command ) free( (void *)config_record->command );
+	if ( config_record->pattern ) free( (void *)config_record->pattern );
+	if ( config_record->source_path ) free( (void *)config_record->source_path );
+	if ( config_record->destination_path ) free( (void *)config_record->destination_path );
+	free( config_record );
+}
+
 config_record_t * read_config( config_t * config )
 {
-	if ( !config )
-	{
-		return NULL;
-	}
+	if ( !config ) return NULL;
 
 	config_record_t * prev_config_record = NULL, * config_record;
 	while ( ( config_record = read_config_record( config ) ) != NULL )
 	{
-		if ( is_not_valid_config_record( config_record ) ) continue;
+		if ( is_not_valid_config_record( config_record ) )
+		{
+			free_config_record( config_record );
+			continue;
+		}
 		config_record->next = prev_config_record;
 		prev_config_record = config_record;
 	}
