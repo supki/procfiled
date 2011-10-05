529db28940065eaa3b547ec0008750c97e923092
Matvey Aksenov <matvey.aksenov@gmail.com>
2011-10-05 18:20:17 +0400

    Add link command processing.

--- src/watches.c
+++ src/watches.c
@@ -13,6 +13,7 @@
 	for(type * item = head; item != NULL; item = item->next)
 
 #define MOVE_COMMAND "move"
+#define LINK_COMMAND "link"
 
 static int inotify_instance = 0;
 static config_record_t * config_head = NULL;
@@ -26,9 +27,10 @@ void init_watches( char * config_file, int inotify_initialized )
 	syslog( LOG_INFO, "Config:" );
 	for_each( config_record_t, config_head, config_record )
 	{
-		if ( !strcmp( config_record->command, MOVE_COMMAND ) )
+		if ( !strcmp( config_record->command, MOVE_COMMAND ) ||
+			!strcmp( config_record->command, LINK_COMMAND ) )
 		{
-			config_record->watch_instance = inotify_add_watch( inotify_instance, config_record->source_path, IN_CREATE | IN_ACCESS | IN_ATTRIB | IN_OPEN );
+			config_record->watch_instance = inotify_add_watch( inotify_instance, config_record->source_path, IN_CREATE );
 		}
 
 		syslog( LOG_INFO, "%d: %s %s from %s to %s", config_record->watch_instance, config_record->command, config_record->pattern, config_record->source_path, config_record->destination_path );
@@ -51,14 +53,28 @@ void parse_inotify_event( struct inotify_event * event )
 
 		if ( !fnmatch( config_record->pattern, event->name, 0 ) )
 		{
-			char * old_path = construct_path( config_record->source_path, event->name );
-			char * new_path = construct_path( config_record->destination_path, event->name );
+			if ( !strcmp( config_record->command, MOVE_COMMAND ) )
+			{
+				char * old_path = construct_path( config_record->source_path, event->name );
+				char * new_path = construct_path( config_record->destination_path, event->name );
 
-			rename( old_path, new_path );
-			syslog( LOG_NOTICE, "moving %s to %s", old_path, new_path );
+				rename( old_path, new_path );
+				syslog( LOG_NOTICE, "moving %s to %s", old_path, new_path );
 
-			destroy_path( old_path );
-			destroy_path( new_path );
+				destroy_path( old_path );
+				destroy_path( new_path );
+			}
+			if ( !strcmp( config_record->command, LINK_COMMAND ) )
+			{
+				char * file_path = construct_path( config_record->source_path, event->name );
+				char * link_path = construct_path( config_record->destination_path, event->name );
+
+				symlink( file_path, link_path );
+				syslog( LOG_NOTICE, "linking %s to %s", link_path, file_path );
+
+				destroy_path( file_path );
+				destroy_path( link_path );
+			}
 		}
 	}
 }
